ac1c78bd54908836be84081193230ba2e7e82cb9
Aksan3467 <62658494+akashraj21@users.noreply.github.com>
2020-07-16 23:34:48 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ForEachLoop {
+    public static void main(String args[]) {
+        int arr[]={12,34,45,56,76};
+        for (int r:arr){
+            System.out.println(r);
+        }
+    }
+}
